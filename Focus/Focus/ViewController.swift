28887ea9677c46b4030674135c05ebd18c5948d8
Mikedan37 <M_Danylchuk@Michaels-MacBook.local>
2017-02-28 17:21:34 -0800

    here

---
+++ Focus/Focus/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Focus
+//
+//  Created by Michael Danylchuk on 2/28/17.
+//  Copyright © 2017 Group. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
