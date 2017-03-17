d9ed68bff024fbc346f6f72bc05026bc7c5ca545
Mikedan37 <M_Danylchuk@michaels-macbook.local>
2017-03-17 14:30:17 -0700

    Added stuff with countdown

--- Focus/Focus/ViewController.swift
+++ Focus/Focus/ViewController.swift
@@ -13,6 +13,8 @@ class ViewController: UIViewController {
     @IBOutlet weak var nameTextField: UITextField!
     @IBOutlet var Start: UIButton!
     @IBOutlet var Time: UILabel!
+    @IBOutlet var not: UILabel!
+
     var t = Timer()
     var minutes: Int = 0
     var seconds: Int = 0
@@ -20,13 +22,13 @@ class ViewController: UIViewController {
     var hours: Int = 0
     var timestring = ""
     var timerison = false
-    @IBOutlet var not: UILabel!
     
     @IBAction func tap(_ sender: Any) {
 //        if timerison == false{
 //            t = Timer.scheduledTimer(timeInterval: 0.01, target: self, selector: #selector(ViewController.update), userInfo: nil, repeats: true)
-        count()
-            t.invalidate()
+//        count()
+           t.invalidate()
+        
         nameTextField.isHidden = false
         timer.isHidden = false
         Start.isHidden = false
@@ -44,6 +46,10 @@ class ViewController: UIViewController {
     
     @IBAction func startButton(_ sender: Any) {
         count()
+//
+       
+       t = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(ViewController.count), userInfo: nil, repeats: true)
+        
         t.fire()
         
         nameTextField.isHidden = true
@@ -54,42 +60,46 @@ class ViewController: UIViewController {
     }
     
     func count(){
-        t = Timer.scheduledTimer(timeInterval: timer.countDownDuration, target: self, selector: #selector(ViewController.update), userInfo: nil, repeats: true)
+        
+         timer.countDownDuration -= 1
         Time.text = String(timer.countDownDuration)
-    }
-    
-    func update(){
         
-        fractions -= 1
-        if fractions == 100{
-            
-            seconds -= 1
-            fractions = 0
-        }
-        
-        if seconds == 60{
-            
-            minutes -= 1
-            seconds = 0
-        }
-        
-        if minutes == 60{
-            
-            hours -= 1
-            minutes = 0
-        }
-    
-        let fractionsString = fractions > 9 ? "\(fractions)" : "0\(fractions)"
-        let secondsString = seconds > 9 ? "\(seconds)" : "0\(seconds)"
-        let minutesString = minutes > 9 ? "\(minutes)" : "0\(minutes)"
-        let hoursString = hours > 9 ? "\(hours)" : "0\(hours)"
-        let timestring = "\(hoursString):\(minutesString):\(secondsString).\(fractionsString)"
-        Time.text = timestring
+//        t = Timer.scheduledTimer(timeInterval: timer.countDownDuration, target: self, selector: #selector(ViewController.update), userInfo: nil, repeats: true)
+//    }
+//    
+//    func update(){
+//        
+//        fractions -= 100
+//        if fractions == 1{
+//            
+//            seconds -= 1
+//            fractions = 0
+//        }
+//        
+//        if seconds == 60{
+//            
+//            minutes -= 1
+//            seconds = 0
+//        }
+//        
+//        if minutes == 60{
+//            
+//            hours -= 1
+//            minutes = 0
+//        }
+//    
+//        let fractionsString = fractions > 9 ? "\(fractions)" : "0\(fractions)"
+//        let secondsString = seconds > 9 ? "\(seconds)" : "0\(seconds)"
+//        let minutesString = minutes > 9 ? "\(minutes)" : "0\(minutes)"
+//        let hoursString = hours > 9 ? "\(hours)" : "0\(hours)"
+//        let timestring = "\(hoursString):\(minutesString):\(secondsString).\(fractionsString)"
+//        Time.text = timestring
     }
 
     override func viewDidLoad() {
         super.viewDidLoad()
      Time.isHidden = true
+        timer.countDownDuration = (00)
         // Do any additional setup after loading the view, typically from a nib.
     }
 
