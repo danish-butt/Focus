bebe4379d71932037ca98afdc7b3c68077e9316d
Mikedan37 <M_Danylchuk@michaels-macbook.local>
2017-03-18 15:56:20 -0700

    Added touch animation

--- Focus/Focus/AnimationViewController.swift
+++ Focus/Focus/AnimationViewController.swift
@@ -9,15 +9,55 @@
 import UIKit
 
 class AnimationViewController: UIViewController {
-
+    
+    
+    var lable = UIView()
+    var Ispressed = false
+    @IBOutlet var Start: UILabel!
+    
+    func animateLable(){
+        UIView.animate(withDuration: 3.0, delay: 0.0, options: .curveEaseOut, animations: {
+            self.Start.alpha = 0.0
+        }, completion: {
+            finished in
+            
+            if finished {
+                //Once the label is completely invisible, set the text and fade it back in
+                self.Start.text = "Touch Anywhere To Begin"
+                
+                // Fade in
+                UIView.animate(withDuration: 3.0, delay: 0.0, options: [.curveEaseIn,.repeat,.autoreverse], animations: {
+                    self.Start.alpha = 1.0
+                }, completion: nil)
+//                    {
+//                    finished in
+//                    
+//                    if finished {
+//                        //Once the label is completely invisible, set the text and fade it back in
+//                        self.Start.text = "Touch Anywhere To Begin"
+//                        
+//                        // Fade in
+//                        UIView.animate(withDuration: 1.0, delay: 0.0, options: .curveEaseIn, animations: {
+//                            self.Start.alpha = 0.0
+//                        }, completion: nil)
+//                    }
+//                })
+            }
+        })
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+    animateLable()
+       
         // Do any additional setup after loading the view.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
+        
         // Dispose of any resources that can be recreated.
     }
     
