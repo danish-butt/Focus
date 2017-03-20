05f3a3ad7e29fd90ac11b1dc08f391446517e6e6
Mikedan37 <M_Danylchuk@michaels-macbook.local>
2017-03-19 19:13:02 -0700

    Added some Alerts

--- Focus/Focus/ViewController.swift
+++ Focus/Focus/ViewController.swift
@@ -25,13 +25,18 @@ class ViewController: UIViewController {
     
     @IBAction func tap(_ sender: Any) {
         t.invalidate()
+        let alertController = UIAlertController(title: nameTextField.text, message:
+            "DONE!🎉", preferredStyle: UIAlertControllerStyle.alert)
+        alertController.addAction(UIAlertAction(title: "save", style: UIAlertActionStyle.default,handler: nil))
+        alertController.addAction(UIAlertAction(title: "dismiss", style: UIAlertActionStyle.default,handler: nil))
+        self.present(alertController, animated: true, completion: nil)
         nameTextField.isHidden = false
         timer.isHidden = false
         Start.isHidden = false
         Time.isHidden = true
         not.isHidden = false
         dis2.isHidden = false
-        dis1.isHidden = false
+        dis1.text = "Write The Task And Hit Start To Start Timer"
     }
     
     
@@ -39,17 +44,14 @@ class ViewController: UIViewController {
         
      t = Timer.scheduledTimer(timeInterval: 60 , target: self, selector: #selector(ViewController.count), userInfo: nil, repeats: true)
         t.fire()
-        if timer.countDownDuration == 0.0{
-            
-        }
-        
+
         nameTextField.isHidden = true
         timer.isHidden = true
         Start.isHidden = true
         Time.isHidden = false
         not.isHidden = true
         dis2.isHidden = true
-        dis1.isHidden = true
+        dis1.text = "When Timer Is Done, Tap It To Stop"
     }
     
     
@@ -57,21 +59,29 @@ class ViewController: UIViewController {
         
          let minute = Int(((timer.countDownDuration/60.0)).rounded())
          let hour = Int((((timer.countDownDuration/60.0)/60.0)).rounded())
+        
          timer.countDownDuration -= 1
         
          if minute >= 120{
-            Time.text = String(hour) + " Hours"
+                Time.text = String(hour) + " Hours " + "0" + " Minutes"
         }
          else if minute >= 60 && minute < 120 {
-            Time.text = String(hour) + " Hour"
+                Time.text = String(hour) + " Hour " + "0" + " Minutes"
         }
          else if minute == 1{
-            Time.text = String(minute) + " Minute"
+            Time.text = "0" + " Hours " + String(minute) + " Minute"
         }
-         else{
-            Time.text = String(minute) + " Minutes"
+         else if minute < 60{
+            Time.text = "0" + " Hours " + String(minute) + " Minutes"
+            
+         }else if timer.countDownDuration == 0{
+            t.invalidate()
+            let alertController = UIAlertController(title: nameTextField.text, message:
+                "DONE!🎉", preferredStyle: UIAlertControllerStyle.alert)
+            alertController.addAction(UIAlertAction(title: "save", style: UIAlertActionStyle.default,handler: nil))
+            alertController.addAction(UIAlertAction(title: "dismiss", style: UIAlertActionStyle.default,handler: nil))
+            self.present(alertController, animated: true, completion: nil)
         }
-        
        }
     
 
